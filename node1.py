ede60ea68fd4cacc187ea63e0a3f63b549035a66
LeoneOdinga <leoneodinga41@gmail.com>
2023-10-18 17:44:40 +0300

    Refining the peer to peer network

--- node1.py
+++ node1.py
@@ -1,17 +1,17 @@
 from peer import Peer
 
 if __name__ == '__main__':
-    # Predefined target peer's address
-    target_host, target_port = '127.0.0.1', 8081
-
     node1 = Peer('127.0.0.1', 8080)
-    node1.start_listening_thread()
-
+    node1.start_listen_and_send_thread()
+    
+    # Predefined target peer's address (Node 2)
+    target_host, target_port = '127.0.0.1', 8081
+    node1.create_connection(target_host, target_port)
+    
     while True:
         try:
             message = input("Enter a message to send (or press Enter to receive messages): ")
             if message:
-                node1.create_connection(target_host, target_port)
                 node1.exchange_data(node1.connections[0], message.encode())
             else:
                 # Code for handling received messages can be added here
--- node2.py
+++ node2.py
@@ -1,17 +1,17 @@
 from peer import Peer
 
 if __name__ == '__main__':
-    # Predefined target peer's address
-    target_host, target_port = '127.0.0.1', 8080
-
     node2 = Peer('127.0.0.1', 8081)
-    node2.start_listening_thread()
+    node2.start_listen_and_send_thread()
+
+    # Predefined target peer's address (Node 1)
+    target_host, target_port = '127.0.0.1', 8080
+    node2.create_connection(target_host, target_port)
 
     while True:
         try:
             message = input("Enter a message to send (or press Enter to receive messages): ")
             if message:
-                node2.create_connection(target_host, target_port)
                 node2.exchange_data(node2.connections[0], message.encode())
             else:
                 # Code for handling received messages can be added here
