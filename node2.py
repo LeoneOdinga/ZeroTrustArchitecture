808a249fdb5511f24de79a072d4e4a19c1d500ff
LeoneOdinga <leoneodinga41@gmail.com>
2023-10-20 20:58:31 +0300

    Testing sending of JSON data between nodes

--- node2.py
+++ node2.py
@@ -6,8 +6,6 @@ from Networking import Networking
 
 node_2 = Networking("127.0.0.2", 8002, 2)
 
-time.sleep(1)
-
 node_2.start()
 
 time.sleep(15)
@@ -16,13 +14,15 @@ debug = False
 
 node_2.connect_with_node('127.0.0.1', 8001)
 
+node_2.debug -debug
+
 time.sleep(2)
 
 node_2.send_to_nodes("Hi there from node 2 to node 1")
 node_2.send_message_to_node('1','HELLOOOOOOOOOOOO')
 node_2.print_all_nodes()
 
-time.sleep(20)
+time.sleep(10)
 
 node_2.stop()
 
