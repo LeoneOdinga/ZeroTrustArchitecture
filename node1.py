c27ddd161d89a7ea19dd3f32c34f07c048eae676
LeoneOdinga <leoneodinga41@gmail.com>
2023-10-20 22:46:39 +0300

    Testing simple main event loop

--- node1.py
+++ node1.py
@@ -1,37 +1,27 @@
 import sys
-import time
+
 sys.path.insert(0, '..') # Import the files where the modules are located
 
 from Networking import Networking
 
+#Create an instance of this node
 node_1 = Networking("127.0.0.1", 8001, 1)
 
+#Start the node
 node_1.start()
-
-<<<<<<< HEAD
-time.sleep(5)
-=======
-time.sleep(10)
->>>>>>> 211b6fde86928bd5315d16ed783858aafd53f9b8
 
 debug = False
 
+#Connect with node 2
 node_1.connect_with_node('127.0.0.2', 8002)
-<<<<<<< HEAD
-node_1.print_connections()
 
-time.sleep(1)
-
+#Start a loop to keep sending messages between node 1 and node 2
 while(True):
-    userInput = input("Send a Message to  Node 2")
+
+    userInput = input("Send a Message to  Node 2: ")
     node_1.send_message_to_node('2',userInput)
 
-time.sleep(1)
-node_1.send_message_to_node('2',"hweeeeeeeeeeeeeey")  
+    if(userInput == 'exit'):
+        break
 
 node_1.stop()
-=======
-node_1.debug =debug
-
-node_1.send_message_to_node('2',{'Name':'leone'})
->>>>>>> 211b6fde86928bd5315d16ed783858aafd53f9b8
--- node2.py
+++ node2.py
@@ -1,37 +1,27 @@
 import sys
-import time
+
 sys.path.insert(0, '..') # Import the files where the modules are located
 
 from Networking import Networking
 
+#Create an instance of this node
 node_2 = Networking("127.0.0.2", 8002, 2)
 
+#Start the node
 node_2.start()
-<<<<<<< HEAD
 
-time.sleep(15)
-
-=======
-time.sleep(10)
->>>>>>> 211b6fde86928bd5315d16ed783858aafd53f9b8
 debug = False
 
-node_2.connect_with_node('127.0.0.1', 8001)
+#Connect with node 2
+node_2.connect_with_node('127.0.0.2', 8002)
 
-node_2.debug = debug
-
+#Start a loop to keep sending messages between node 1 and node 2
 while(True):
-    userInput = input("Send a Message to  Node 1")
-    node_2.send_message_to_node('1',userInput)
 
-<<<<<<< HEAD
-time.sleep(20)
-=======
+    userInput = input("Send a Message to  Node 2: ")
+    node_2.send_message_to_node('2',userInput)
+
     if(userInput == 'exit'):
         break
-time.sleep(10)
->>>>>>> 211b6fde86928bd5315d16ed783858aafd53f9b8
 
 node_2.stop()
-
-print('end test')
