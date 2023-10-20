92197c5d952fb4ed65663ef1e8bea13c211608c6
LeoneOdinga <leoneodinga41@gmail.com>
2023-10-20 22:03:34 +0300

    Tested Simple Main Event Loop for sending an receiving requests/responses

--- node1.py
+++ node1.py
@@ -6,24 +6,21 @@ from Networking import Networking
 
 node_1 = Networking("127.0.0.1", 8001, 1)
 
-time.sleep(1)
-
 node_1.start()
 
-time.sleep(5)
-
-debug = True
+debug = False
 
 node_1.connect_with_node('127.0.0.2', 8002)
 node_1.debug =debug
 
-time.sleep(1)
-node_1.print_all_nodes()
-
-time.sleep(1)
 node_1.send_message_to_node('2',{'Name':'leone'})
 
-time.sleep(10)
-node_1.stop()
 
-print('end test')
+while(True):
+    userInput = input("Send a Message to  Node 2")
+    node_1.send_message_to_node('2',userInput)
+
+    if(userInput == 'exit'):
+        break
+
+node_1.stop()
--- node2.py
+++ node2.py
@@ -8,21 +8,18 @@ node_2 = Networking("127.0.0.2", 8002, 2)
 
 node_2.start()
 
-time.sleep(15)
-
-debug = True
+debug = False
 
 node_2.connect_with_node('127.0.0.1', 8001)
 
 node_2.debug = debug
 
-time.sleep(2)
+while(True):
+    userInput = input("Send a Message to  Node 1")
+    node_2.send_message_to_node('1',userInput)
 
-node_2.send_to_nodes("Hi there from node 2 to node 1")
-node_2.send_message_to_node('1','HELLOOOOOOOOOOOO')
-node_2.print_all_nodes()
-
-time.sleep(10)
+    if(userInput == 'exit'):
+        break
 
 node_2.stop()
 
