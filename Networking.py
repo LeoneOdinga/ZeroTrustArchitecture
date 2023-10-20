ca52ca27604f32a8632d24d1705924b9dbef95be
LeoneOdinga <leoneodinga41@gmail.com>
2023-10-20 11:37:46 +0300

    Added NODE ROLES and function to get the node role from the node.id attribute

--- Networking.py
+++ Networking.py
@@ -1,4 +1,5 @@
 '''
+
 Class that extends the p2pnetwork class to add application specific implementation Details 
 Handles how communication happens between the access proxy, trust engine, and policy engine.
 Uses peer to peer communication without involvement of a centralized server for establishing connections
@@ -8,12 +9,23 @@ Uses peer to peer communication without involvement of a centralized server for 
 from p2pnetwork.node import Node
 
 class Networking(Node):
+    #Define a dictionary of the node roles based on their node.id attributes
+    NODE_ROLE = {
+        '1': 'Access Proxy Node',
+        '2':'Trust Engine Node',
+        '3':'Policy Engine Node'
+    }
 
     # Python class constructor to initialize the class 
     def __init__(self, host, port, id=None, callback=None, max_connections=0):
         super(Networking, self).__init__(host, port, id, callback, max_connections)
         print("Starting...")
     
+    #Define a function to extract the name of a node based on it's node.id attribute
+    def get_node_role(self,node_id):
+        return self.NODE_ROLE.get(node_id,'UNKNOWN ROLE')
+    
+
     def send_message_to_node(self, node_id, message):
     # Find the specific node by its ID
         target_node = None
@@ -28,6 +40,7 @@ class Networking(Node):
             else:
                 print(f"Node {node_id} not found.")
     
+
     def print_all_nodes(self):
         print("Outbound Nodes:")
         for node in self.nodes_outbound:
@@ -37,11 +50,13 @@ class Networking(Node):
         for node in self.nodes_inbound:
             print(f"Inbound Node ID: {node.id}, Host: {node.host}, Port: {node.port}")
 
-    # all the methods below are called when things happen in the network.
-    # implement your network node behavior to create the required functionality.
+
+    # The methods below are called when events happen in the network
 
     def outbound_node_connected(self, node):
+        node_role = self.get_node_role(node.id)
         print("outbound_node_connected (" + self.id + "): " + node.id)
+        print(f"Connected to Outbound node: {node_role}")
         
     def inbound_node_connected(self, node):
         print("inbound_node_connected: (" + self.id + "): " + node.id)
