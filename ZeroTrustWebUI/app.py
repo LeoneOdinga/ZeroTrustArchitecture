d05b44630731b821972d7b6fd65be9a93d90da1f
LeoneOdinga <leoneodinga41@gmail.com>
2023-10-23 00:13:08 +0300

    Testing sending of request to flask backend using fetch API

--- ZeroTrustWebUI/app.py
+++ ZeroTrustWebUI/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +9,15 @@ def index():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+#route to receive an access request and forward it to the AP | Testing...
+
+@app.route('/receive-request', methods = ['POST'])
+def receive_and_process_access_request():
+    #receive the data from the front end when the option1 is clicked. 
+    data = request.json
+    print("Received data:", data)
+    return jsonify({'message': 'Data receivved successfully'})
 
 if __name__ == "__main__":
     app.run(debug=True)
