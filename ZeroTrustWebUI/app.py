fa89a2cdf1493ea7772af8f244bf9342f1c824e3
LeoneOdinga <leoneodinga41@gmail.com>
2023-10-22 23:48:25 +0300

    Testing home route and rendering ZTA page

--- ZeroTrustWebUI/app.py
+++ ZeroTrustWebUI/app.py
@@ -3,9 +3,12 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html', message = "ZT Home Page!")
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
