52c6da2c7160e02a9ff8ae56a2d3c246ea99e3ce
LeoneOdinga <leoneodinga41@gmail.com>
2023-11-07 23:25:49 +0300

    PAM implementation rollout

--- ZeroTrustWebUI/app.py
+++ ZeroTrustWebUI/app.py
@@ -1,3 +1,4 @@
+import json
 import sys
 from flask import Flask,render_template, request, jsonify, session, url_for,redirect, make_response
 import logging
@@ -6,7 +7,8 @@ from flask import Flask, g
 from flask_oidc import OpenIDConnect
 from keycloak import KeycloakAuthenticationError, KeycloakOpenID
 import time
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import requests
 from Networking import Networking
 
@@ -56,6 +58,34 @@ keycloak_openid = KeycloakOpenID(server_url="http://localhost:8080/auth/",
                                  client_id="ZeroTrustPlatform",
                                  realm_name="myrealm",
                                  client_secret_key="KEViiP0yTFDgjfxKee2Xg1hgaCDHAEqU")
+
+# Configuration for SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///privileged_access.db' 
+db = SQLAlchemy(app)
+
+# Define the database model for access requests
+class AccessRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    resource_name = db.Column(db.String(100), nullable=False)
+    reason_for_access = db.Column(db.String(250), nullable=False)
+    access_duration = db.Column(db.Integer, nullable=False)
+    requestor_id = db.Column(db.String(100), nullable=False)
+    requestor_username = db.Column(db.String(100), nullable=False)
+    time_of_request = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    requestStatus = db.Column(db.String(20), default="pending")
+    approvers = db.Column(db.String(500), nullable=False)
+    approvers = db.relationship('Approver', back_populates='request')
+
+# Create a new database model for approvers
+class Approver(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    approverID = db.Column(db.String(100), nullable=False)
+    approverEmail = db.Column(db.String(100), nullable=False)
+    request_id = db.Column(db.Integer, db.ForeignKey('access_request.id'), nullable=False)
+    request = db.relationship('AccessRequest', back_populates='approvers')
+    approver_secret_share = db.Column(db.String(750))
+
 
 '''
 
@@ -135,6 +165,28 @@ def extract_user_role():
     resource_access = introspection_result.get('resource_access', {}).get('ZeroTrustPlatform', {})
     user_roles = resource_access.get('roles', [])
     return user_roles
+
+#function to construct shares from a secret key
+import tss,base64
+def generate_and_reconstruct_secret(threshold, num_shares, secret, identifier):
+    # Generate shares
+    shares = tss.share_secret(threshold, num_shares, secret, identifier, tss.Hash.SHA256)
+
+    # Encode shares in Base64
+    base64_shares = [base64.b64encode(share).decode() for share in shares]
+
+
+    print(base64_shares)
+
+    # Reconstruct the secret from Base64-encoded shares
+    binary_shares = [base64.b64decode(share.encode()) for share in base64_shares]
+
+    try:
+        # Recover the secret value
+        reconstructed_secret = tss.reconstruct_secret(binary_shares)
+        return reconstructed_secret
+    except tss.TSSError:
+        return None  # Handling error
 
 # The home route where all the available services are located
 @app.route('/home')
@@ -217,7 +269,77 @@ def resource_selection():
 
     return render_template('resourceSelection.html',user_id=user_id,username=username,given_name=given_name)
 
+from datetime import datetime  # Import the datetime module
+
+@app.route('/privilegedAccess', methods=['GET', 'POST'])
+def privilegedAccess():
+    if request.method == 'POST':
+        # Get form data
+        resource_name = request.form['resource_name']
+        reason_for_access = request.form['reason_for_access']
+        access_duration = int(request.form['access_duration'])
+        # Get the current user's ID and username from the session
+        requestor_id = session['oidc_auth_profile'].get('sub')
+        requestor_username = session['oidc_auth_profile'].get('preferred_username')
+        # Capture the time of the request
+        time_of_request = datetime.now()
+        # Extract selected approvers (approvers is now a list)
+        selected_approvers = request.form.getlist('approvers')
+        # Convert the list of approvers to a JSON string
+        #approvers_json = json.dumps(selected_approvers)  # Convert to JSON string
+
+        # Validate the access duration (between 1 and 100 minutes)
+        if 1 <= access_duration <= 100:
+            # Create a new access request and add it to the database
+            new_request = AccessRequest(
+                resource_name=resource_name,
+                reason_for_access=reason_for_access,
+                access_duration=access_duration,
+                requestor_id=requestor_id,
+                requestor_username=requestor_username,
+                time_of_request=time_of_request,
+                requestStatus='pending',  # Set the requestStatus to 'pending'
+                approvers=",".join(selected_approvers)
+            )
+            db.session.add(new_request)
+            db.session.commit()
+
+            return render_template('success.html', message="Successfully received your request! Processing...")
+        else:
+            return "Invalid access duration. Please enter a value between 1 and 100 minutes."
+
+    return render_template('privilegedAccessManagement.html')
+
+# Define a variable to store the generated secret
+generated_secret = "TEST"
+
+@app.route('/processSecretKey', methods=['GET', 'POST'])
+def process_secret_key():
+    global generated_secret
+
+    if request.method == 'POST':
+        # Get the secret key entered by the user
+        entered_secret_key = request.form.get('secret_key')
+
+        if generated_secret is not None and entered_secret_key == generated_secret:
+            # Redirect to the hidden page if the secret key matches
+            return redirect(url_for('hidden_page'))
+        else:
+            # If the secret key doesn't match, display an error message
+            return "Error: Invalid secret key"
+
+    return render_template('enterKey.html')
+
+@app.route('/viewAccessRequests')
+def view_access_requests():
+    access_requests = AccessRequest.query.all()
+    requestor_id = session['oidc_auth_profile'].get('sub')
+    print(requestor_id)
+
+    return render_template('viewAccessRequests.html', access_requests=access_requests)
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
 
